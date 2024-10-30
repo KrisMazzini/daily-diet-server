3e2b5cee6355e34fe746a9f8c961442267627e95
Kristopher Mazzini <kristophermazzini.sc@gmail.com>
2024-10-30 20:42:07 -0300

    feat: create users table and routes

---
+++ src/routes/users.ts
@@ -0,0 +1,48 @@
+import { randomUUID } from 'node:crypto'
+import { FastifyInstance } from 'fastify'
+import { z } from 'zod'
+
+import { knex } from '../database'
+
+export async function userRoutes(app: FastifyInstance) {
+  app.get('/', async () => {
+    const users = await knex('users').select('*')
+
+    return { users }
+  })
+
+  app.post('/', async (request, response) => {
+    const createUserSchema = z.object({
+      name: z.string().trim().min(1),
+      email: z.string().email(),
+    })
+
+    const { success, data: body } = createUserSchema.safeParse(request.body)
+
+    if (!success) {
+      return response.status(400).send({ message: 'Invalid body.' })
+    }
+
+    const { name, email } = body
+
+    const existingUser = await knex('users').where({ email }).first()
+
+    if (existingUser) {
+      return response.status(400).send({ message: 'E-mail already exists.' })
+    }
+
+    const sessionId = randomUUID()
+
+    response.cookie('sessionId', sessionId, {
+      path: '/',
+      maxAge: 60 * 60 * 24 * 7, // 7 days
+    })
+
+    await knex('users').insert({
+      id: randomUUID(),
+      name,
+      email,
+      session_id: sessionId,
+    })
+  })
+}
